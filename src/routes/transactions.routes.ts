166562512d83ecedf5c6543f03f6d8e649194cbc
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-10-31 09:30:11 -0300

    feat: should be able to delete a transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import CreateTransactionService from '../services/CreateTransactionService';
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -36,7 +36,12 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+  await deleteTransactionService.execute({ id });
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
