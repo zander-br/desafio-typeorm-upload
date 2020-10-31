15dc41e508f3ee43b6201f91128172771e3b52ed
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-10-31 09:18:12 -0300

    feat: list all transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import CreateTransactionService from '../services/CreateTransactionService';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {});
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find({
+    relations: ['category_id'],
+  });
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({
+    transactions,
+    balance,
+  });
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
